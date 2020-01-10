de9734ac5579845cf4fb2f4585347c0b8f4971ee
bghull <35785950+BaneJammin@users.noreply.github.com>
2020-01-09 20:26:26 -0500

    Add tests

--- campsight/campsight.py
+++ campsight/campsight.py
@@ -1,74 +1,75 @@
-#TODO:
-#   check input date is within next 16 days (api limitation)
-#   handle one-digit day and month input for API return checking
-
 import bs4
-import config
-import datetime
 import json
 import requests
+import datetime
 
-base_url = config.base_url
-postal_code = config.postal_code
-api_key = config.weatherbit_key
-api_url = config.weatherbit_api
 
-def date_to_dict(dateform, prefix=''):
-    return {f'{prefix}month': dateform.month,
-            f'{prefix}day': dateform.day,
-            f'{prefix}year': dateform.year}
+def date_to_dict(dateform, prefix=""):
+    return {
+        f"{prefix}month": dateform.month,
+        f"{prefix}day": dateform.day,
+        f"{prefix}year": dateform.year,
+    }
+
 
 def dict_to_date(dictform):
-    return datetime.date(dictform['year'],
-                         dictform['month'],
-                         dictform['day'])
+    return datetime.date(dictform["year"], dictform["month"], dictform["day"])
+
 
 def yield_params() -> dict:
-    prompt = {'month': None, 'day': None, 'year': None, 'window': None}
+    prompt = {"month": None, "day": None, "year": None, "window": None}
     for p in prompt:
-        prompt[p] = int(input(f'{p.capitalize()}? '))
-    for i in range(prompt['window']):
+        prompt[p] = int(input(f"{p.capitalize()}? "))
+    for i in range(prompt["window"]):
         start_date = dict_to_date(prompt) + datetime.timedelta(days=i)
         end_date = start_date + datetime.timedelta(days=1)
-        yield {**date_to_dict(start_date),
-               **date_to_dict(end_date, prefix='e'),
-               'find_pk': 1}
+        yield {
+            **date_to_dict(start_date),
+            **date_to_dict(end_date, prefix="e"),
+            "find_pk": 1,
+        }
+
 
 def yield_websoup() -> tuple:
     for params in yield_params():
         res = requests.get(base_url, params=params)
-        soup = bs4.BeautifulSoup(res.text, features='html.parser')
-        yield params['month'], params['day'], params['year'] , soup
+        soup = bs4.BeautifulSoup(res.text, features="html.parser")
+        yield params["month"], params["day"], params["year"], soup
+
 
 def parse_soup() -> dict:
     result = {}
     for m, d, y, soup in yield_websoup():
         available_sites = []
-        for i in soup.find_all('table'):
-            rows = i.find_all('tr')
-            name = rows[0].td.text[:rows[0].td.text.find(':')]
-            status = rows[2].find_all('td')[1].text
-            if status == ' Reserve now':
+        for i in soup.find_all("table"):
+            rows = i.find_all("tr")
+            name = rows[0].td.text[: rows[0].td.text.find(":")]
+            status = rows[2].find_all("td")[1].text
+            if status == " Reserve now":
                 available_sites.append(name)
-        result[f'{y}-{m}-{d}'] = available_sites
+        result[f"{y}-{m}-{d}"] = available_sites
     for date in result:
         if not result[date]:
-            result[date] = ['None available']
+            result[date] = ["None available"]
     return result
 
+
 def pull_forecast() -> dict:
-    params = {'postal_code': postal_code, 'country': 'US', 'units': 'I', 'key': api_key}
+    params = {"postal_code": postal_code, "country": "US", "units": "I", "key": api_key}
     res = requests.get(api_url, params=params)
     weather = json.loads(res.text)
-    weather = {day['datetime']: day for day in weather['data']}
+    weather = {day["datetime"]: day for day in weather["data"]}
     return weather
- 
+
+
 def compare_dates():
     parks = parse_soup()
     weather = pull_forecast()
     for date in parks:
         if date in weather:
-            print(f"\n{date}\n",
+            print(
+                f"\n{date}\n",
                 f"Low: {weather[date]['app_min_temp']}\n",
-                    f"Chance of precip: {weather[date]['pop']}%\n",
-                        parks[date])
+                f"Chance of precip: {weather[date]['pop']}%\n",
+                parks[date],
+            )
